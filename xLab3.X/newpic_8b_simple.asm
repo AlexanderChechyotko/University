79ca369a8ff091c8630a57f6882a0aa856468db2
Alexandr Chechyotko <Alexandr Chechyotko>
2017-03-10 21:08:03 +0300

    add 2 loops

--- xLab3.X/newpic_8b_simple.asm
+++ xLab3.X/newpic_8b_simple.asm
@@ -5,18 +5,37 @@ portb	equ	06h
 trisb	equ	06h
 value	equ	0x2F
 n	set	b'00010000'
- L1        EQU 0X14
- L2         EQU 0X15
+flag	equ	0x09
+L1	EQU	0X14
+L2	EQU	0X15
 
 	org 0
 	clrf	portb
 	bsf	status,5
 	clrf	trisb
 	bcf	status,5
+	bsf	portb,7
+
+click	btfsc       portb,7
+	goto        continue 
+	goto	change
+change
+	btfsc	flag,0
+	incf	flag
+	decf	flag
+
+	bsf	portb,7
+
+continue:
+	btfss	flag,0
+	goto	loop_right
+	goto	loop_left
 init:
-	movlw	0x1
+	movlw	1
 	movwf	value
-loop1:
+	movlw	1
+	movwf	flag
+loop_right:
 	call	delay
 	movf	value,0
 	movwf	portb
@@ -26,8 +45,9 @@ loop1:
 	movf	value,0
 	sublw	n
 	btfss	status,2
-	goto	loop1
-loop2:
+	goto	loop_right
+	goto	init
+loop_left:
 	call	delay
 	movf	value,0
 	movwf	portb
@@ -37,20 +57,31 @@ loop2:
 	movf	value,0
 	sublw	0
 	btfss	status,2
-	goto	loop2
+	goto	loop_left
 	goto	init
 
+
+
+
+
+
+
+
+
+
+
+
 delay:
-	                         ; Time Delay Routines
-     MOVLW 50                        ; Copy 50 to W
-     MOVWF L2                    ; Copy W into L2
+	                         
+     MOVLW 50                       
+     MOVWF L2                    
  LOOP2
-     MOVLW 255                   ; Copy 255 into W
-     MOVWF L1                    ; Copy W into L1
+     MOVLW 255                  
+     MOVWF L1                    
  LOOP1
-     decfsz L1,F                    ; Decrement L1. If 0 Skip next instruction
-         GOTO LOOP1                ; ELSE Keep counting down
-     decfsz L2,F                    ; Decrement L2. If 0 Skip next instruction
-         GOTO LOOP2                ; ELSE Keep counting down
+     decfsz L1,F                    
+         GOTO LOOP1                
+     decfsz L2,F                    
+         GOTO LOOP2                
      Return
 	end
